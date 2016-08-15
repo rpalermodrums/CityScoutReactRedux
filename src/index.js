ddb0fd812eb32e6adbbf4ca6344edef4985f2ba2
laushinka <laurie.malau@gmail.com>
2016-08-14 21:31:24 -0400

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import cityScoutApp from './reducers/index'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import ReduxPromise from 'redux-promise'
 import './index.css';
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(cityScoutApp)}>
+    <App />
+  <Provider />,
   document.getElementById('root')
 );
