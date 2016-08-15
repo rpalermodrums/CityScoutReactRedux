2802801cf8f299c1d73e36695aedabb8fcfa185c
laushinka <laurie.malau@gmail.com>
2016-08-14 22:02:51 -0400

    Add rootReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+// Mapping of state
+const rootReducer = combineReducers({
+  // state: reducer
+  // crimes: CrimesReducer,
+  // parks: ParksReducer,
+  // schools: SchoolsReducer,
+  // subways: SubwaysReducer
+});
+
+export default rootReducer;
