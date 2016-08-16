5322f17526cf71b01bdaae54c303b21c3dd27105
laushinka <laurie.malau@gmail.com>
2016-08-16 10:34:34 -0400

    First draft

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Route, Router, IndexRoute} from 'react-router'
+import App from './app'
+
+const Routes = {
+  <Router>
+    <Route path="/" component={} />
+  </Router>
+}
