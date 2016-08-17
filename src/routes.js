b3ba0e575dd9a3da52f3ab20c672e68d31fbbe3a
JulieMarin <jfm7391@gmail.com>
2016-08-17 12:01:41 -0400

    Edit routes

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import {Route, Router, IndexRoute} from 'react-router'
 import App from './App'
 import SubmitAddressContainer from './containers/submitAddressContainer'
 import PreferencesSliderContainer from './containers/preferencesSlidersContainer'
+import ScoresResult from './containers/ScoresResult'
 
 const Routes = (
   <Router>
     <Route path="/" component={SubmitAddressContainer} />
     <Route path="/preferences" component={PreferencesSliderContainer}>
-      <Route path ="results"  />
+      <Route path ="results" component={ScoresResult} />
     </Route>
   </Router>
 )
