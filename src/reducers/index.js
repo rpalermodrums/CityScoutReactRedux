6d61995700cb45c27e584c064691a2c59b63a939
laushinka <laurie.malau@gmail.com>
2016-08-17 16:18:11 -0400

    Add scores reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,7 @@ import searchReducer from './searchReducer'
 // Mapping of state
 const rootReducer = combineReducers({
   address: searchReducer
-  // crimes: CrimesReducer,
-  // parks: ParksReducer,
-  // schools: SchoolsReducer,
-  // subways: SubwaysReducer
+  preferences: scoresReducer
 });
 
 export default rootReducer;
