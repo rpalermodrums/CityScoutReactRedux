9856fbe8d4f56e606a39949f8bb28e0ecba274f1
laushinka <laurie.malau@gmail.com>
2016-08-18 16:15:08 -0400

    Create preference page component for PreferencesSliderContainer

---
+++ src/components/preferencePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PreferencesSliderContainer from '../containers/preferencesSlidersContainer'
+
+export default function() {
+  return(
+    <div id="container">
+      <div id="preference-bar">
+        <PreferencesSliderContainer />
+        <div className="">
+        </div>
+        <div className="">
+        </div>
+      </div>
+    </div>
+  )
+}
