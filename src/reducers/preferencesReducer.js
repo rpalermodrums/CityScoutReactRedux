6f596c64f8f469e5b33680f413c9449e99f4de98
laushinka <laurie.malau@gmail.com>
2016-08-18 16:13:52 -0400

    No change

--- src/reducers/preferencesReducer.js
+++ src/reducers/preferencesReducer.js
@@ -2,7 +2,6 @@ export default function preferencesReducer(state = {}, action) {
 
   switch (action.type) {
     case 'GET_PREFERENCES':
-      debugger
       return Object.assign({}, state, {preferences: action.payload})
     default:
       return state
