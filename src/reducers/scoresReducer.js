4b740b5a67178b4bf0f95e8a38cee16ad5b8a1f1
laushinka <laurie.malau@gmail.com>
2016-08-18 16:12:04 -0400

    No change

--- src/reducers/scoresReducer.js
+++ src/reducers/scoresReducer.js
@@ -1,7 +1,6 @@
 export default function scoresReducer(state = {}, action) {
   switch (action.type) {
     case 'FETCH_SCORES':
-    debugger
       return Object.assign({}, state, {scores: action.payload})
     default:
       return state
--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,7 +1,6 @@
 export default function searchReducer(state = {}, action){
   switch(action.type){
     case 'GET_ADDRESS':
-      debugger
       return Object.assign({}, state, {address: action.payload})
     default:
       return state
