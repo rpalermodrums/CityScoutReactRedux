5a1511262135d81e549d91505695a4f64a6379eb
laushinka <laurie.malau@gmail.com>
2016-08-18 15:11:36 -0400

    Create landing page

---
+++ src/components/landingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SubmitAddressContainer from '../containers/submitAddressContainer'
+
+export default function() {
+  return(
+    <div id="landing-page">
+      <h1>Welcome to CityScout NYC</h1>
+      <SubmitAddressContainer />
+    </div>
+  )
+}
