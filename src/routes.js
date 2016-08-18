a0d6efa9951ca5ead1a289f89382a622fed73074
laushinka <laurie.malau@gmail.com>
2016-08-18 16:12:33 -0400

    Edit route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Route, Router} from 'react-router'
-import SubmitAddressContainer from './containers/submitAddressContainer'
+import LandingPage from './components/landingPage'
 import PreferencesSliderContainer from './containers/preferencesSlidersContainer'
 import ScoresResult from './containers/ScoresResult'
 
 const Routes = (
   <Router>
-    <Route path="/" component={SubmitAddressContainer} />
+    <Route path="/" component={LandingPage} />
     <Route path="/preferences" component={PreferencesSliderContainer} />
     <Route path="/results" component={ScoresResult} />
   </Router>
