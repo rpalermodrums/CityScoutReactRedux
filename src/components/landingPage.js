ca4594c2664ca393c2bada483f39801f9487420d
laushinka <laurie.malau@gmail.com>
2016-08-18 16:53:52 -0400

    Add divs

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -3,10 +3,12 @@ import SubmitAddressContainer from '../containers/submitAddressContainer'
 
 export default function() {
   return(
-    <div id="container">
-      <div id="landing-page">
-        <h1>Welcome to CityScout NYC</h1>
-        <SubmitAddressContainer />
+    <div className="ink-grid" id="container">
+      <div className="column-group">
+        <div id="landing-page" className="all-100">
+          <h1>Welcome to CityScout NYC</h1>
+          <SubmitAddressContainer />
+        </div>
       </div>
     </div>
   )
