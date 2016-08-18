5a3424d3c02c81186e4391d05dcf0cd24977fe8a
laushinka <laurie.malau@gmail.com>
2016-08-18 16:21:14 -0400

    Create result page

---
+++ src/components/resultPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScoresResult from '../containers/ScoresResult'
+
+export default function() {
+  return(
+    <div id="container">
+      <div id="result-page">
+        <ScoresResult />
+      </div>
+    </div>
+  )
+}
