5fe6619b520236ee66e6754250ae31c13cf3d215
Ryan Palermo <stix250@gmail.com>
2016-08-20 17:19:15 -0400

    Edit styling

--- src/components/preferencePage.js
+++ src/components/preferencePage.js
@@ -4,26 +4,26 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export default function() {
   return(
-    <ReactCSSTransitionGroup
-      component="div"
-      className="ink-grid"
-      transitionName="route"
-      transitionEnterTimeout={600}
-      transitionAppearTimeout={600}
-      transitionLeaveTimeout={400}
-      transitionAppear={true}>
-      <div className="column-group">
-      <div className="slider-title">
-        <h3>Choose your neighborhood categories</h3>
-      </div>
-        <div id="preference-bar">
-          <PreferencesSliderContainer />
-          <div className="">
-          </div>
-          <div className="">
-          </div>
-        </div>
-      </div>
-    </ReactCSSTransitionGroup>
+    <div className="container">
+        <ReactCSSTransitionGroup
+          className="preferences-box"
+          component="div"
+          transitionName="route"
+          transitionEnterTimeout={600}
+          transitionAppearTimeout={600}
+          transitionLeaveTimeout={400}
+          transitionAppear={true}>
+            <div className="slider-title col-md-12">
+              <h3>Choose your neighborhood categories</h3>
+            </div>
+            <div className="preference-bar">
+              <PreferencesSliderContainer />
+              <div className="">
+              </div>
+              <div className="">
+              </div>
+            </div>
+        </ReactCSSTransitionGroup>
+    </div>
   )
 }
