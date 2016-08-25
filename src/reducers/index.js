895cfefcbfd91950cb1d27ca05490ecb3e28731a
laushinka <laurie.malau@gmail.com>
2016-08-25 11:14:41 -0400

    Add locationsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import searchReducer from './searchReducer'
 import scoresReducer from './scoresReducer'
 import preferencesReducer from './preferencesReducer'
+import locationsReducer from './locationsReducer'
 
 // Mapping of state
 const rootReducer = combineReducers({
   address: searchReducer,
   preferences: preferencesReducer,
-  scores: scoresReducer
+  scores: scoresReducer,
+  coords: locationsReducer
 });
 
 export default rootReducer;
