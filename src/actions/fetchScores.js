e34e3f2b4e807e72643d3af40b7f8725bc6126ce
laushinka <laurie.malau@gmail.com>
2016-09-13 18:53:49 -0400

    Update API url

--- src/actions/fetchScores.js
+++ src/actions/fetchScores.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 function fetchScores(address){
 
-  const url = `http://localhost:3001/api/v1/voting_districts?full_address=${address}`
+  const url = `https://city-scout.herokuapp.com/api/v1/voting_districts?full_address=${address}`
 
   const response = Axios.get(url)
 
